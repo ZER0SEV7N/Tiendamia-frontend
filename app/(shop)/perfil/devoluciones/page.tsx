b9aef23af7160ae1c8ada3ab04ff36f2925589f5
Edson5Leonard <edsonleonardorojascabia@gmail.com>
2026-05-24 01:47:08 -0500

    feat: Subir vista de Info, Mis ordenes y Devoluciones

---
+++ app/(shop)/perfil/devoluciones/page.tsx
@@ -0,0 +1,9 @@
+import DevolucionesView from "./views/devolucionesView";
+
+export const metadata = {
+  title: "Tus solicitudes de devoluciones - Tiendamia",
+};
+
+export default function DevolucionesPage() {
+  return <DevolucionesView />;
+}
---
+++ app/(shop)/perfil/ordenes/page.tsx
@@ -0,0 +1,9 @@
+import MisOrdenesView from "./views/MisOrdenesView";
+
+export const metadata = {
+  title: "Mis Órdenes - Tiendamia",
+};
+
+export default function MisOrdenesPage() {
+  return <MisOrdenesView />;
+}
