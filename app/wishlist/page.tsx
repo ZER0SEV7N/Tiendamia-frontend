117988375674a2df072b3bae117482f734faa3f7
YandelRamos <yandelramos67@gmail.com>
2026-05-28 21:39:24 -0500

    cambios jueves

---
+++ app/wishlist/page.tsx
@@ -0,0 +1,18 @@
+import SidebarCuenta from "@/components/cuenta/SidebarCuenta";
+import Wishlist from "@/components/cuenta/Wishlist";
+
+export default function Page() {
+
+  return (
+
+    <div className="flex">
+
+      <SidebarCuenta />
+
+      <Wishlist />
+
+    </div>
+
+  )
+
+}
